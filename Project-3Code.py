08241302f2def5b548da5e1886eba64687f22d4e
Nicholas Beach <nicholasallenbeach@gmail.com>
2018-03-05 07:12:59 -0600

    update
    U

--- Project-3Code.py
+++ Project-3Code.py
@@ -1,138 +1,102 @@
+from datetime import datetime
 from urllib.request import urlretrieve
-import datetime
 import re
 URL_PATH = 'https://s3.amazonaws.com/tcmg476/http_access_log'
-LOCAL_FILE = 'local_copy.log'
+log = 'local_copy.log'
 
-local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
+local_file, headers = urlretrieve(URL_PATH, log)
 
 TotalNumber = 0
-log = 'local_copy.log'
+
 errors = 0
 redirects = 0
-Monthdict = {"Jan":0, "Feb":0, "Mar":0, "Apr":0, "May":0, "Jun":0, "Jul":0, "Aug":0, "Sep":0, "Oct":0, "Nov":0, "Dec":0}
+
+#Additional lists and dictionaries allow me to generate outputs with the names of the months attached.
+monthlist= [0, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+oldmonth = {1:"January",2:"February",3:"March",4:"April",5:"May",6:"June",7:"July",8:"August",9:"September",10:"October",11:"November", 12:"December"}
+
+Monthdict = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0}
+Monthrepo = {1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], 10:[], 11:[], 12:[]}
 Daytracker ={"Monday":0,"Tuesday":0,"Wednesday":0,"Thursday":0,"Friday":0,"Saturday":0,"Sunday":0}
-mostrequested={"test":0}
+popularity ={}
+weekcounter = 0
+weekstart = 4
 
+#I made my own regex that splits each line into seven parts
 
+splitter = re.compile('([0-9]+[/]+[A-Z]+[a-z]+[/]+\d\d\d\d)(.*GET)(.+?)(HTTP)(.*") ([2-5])')
 
-'''the initial for loop runs through every line of the log file. The subsequent 
-code is mostly reliant on this particular for loop
-The second loop runs through each month of the year and appends each month's data
-to its appropriate file. Furthermore, it replaces the three letter abbreviation 
-for each month so that the date.time function can pull up the specific day of the week
-the program was run. I tried to use a dictionary for the replacement, however, Apr
-and Oct could not be replaced by April and October. After verifying that it worked for
-everything else, I had write out an if statement for each month instead. ''' 
 for line in open(log):
-            
-    TotalNumber += 1
-    if '" 4' in line:
-        errors+=1
-    if '" 3' in line:
-        redirects+=1
+  TotalNumber +=1
+  seg = splitter.split(line)
+  if not set or len(seg) < 7:
+    continue
+  if seg[6][0] == '3':
+    redirects += 1
+  
+  if seg[6][0] == '4':
+    errors += 1
+  if seg[3] not in popularity.keys():
+    popularity[seg[3]] =1
+  else: 
+    popularity[seg[3]] +=1
+
+  date = datetime.strptime(seg[1], "%d/%b/%Y")
+  day = (date.strftime('%A'))
+  daynumber =(date.strftime('%d'))
+  daymonth =(date.strftime('%b'))
+  weekno = (int(daynumber) - 1) // 7 + 1
+  
+  
+  
+  if weekno == weekstart:    
+    weekcounter +=1
     
+  else:
+    print("%s, week %d, has %d requests " %(daymonth, weekno, weekcounter))
+    weekcounter = 0
+    weekstart = weekno
 
-    for original in Monthdict.keys():
-      
-        if original in line:
-            Monthdict[original] += 1
-            textfile = open(original+".txt", "a")
-            textfile.write("%s\n" %(line))
-            textfile.close      
-            x = line[line.find("[")+1:line.find(":")]
-                    
-            if "January" in x:
-                continue
-            
-            elif "Jan" in x:
-                x = x.replace("Jan", "January")
-            if "February" in x:
-                continue
-            elif "Feb" in x:
-                x = x.replace("Feb", "February")
-            if "March" in x:
-                continue                    
-            
-            elif "Mar" in x:
-                x = x.replace("Mar", "March")
-            if "April" in x:
-                continue                    
-            
-            elif "Apr" in x:
-                x = x.replace("Apr", "April")
-            if "May" in x:
-                continue                    
-                                           
-            elif "May" in x:
-                x = x.replace("May", "May")
-            if "June" in x:
-                continue                
-                            
-            elif "Jun" in x:
-                x = x.replace("Jun", "June")     
-            if "July" in x:
-                continue                    
-                    
-            elif "Jul" in x:
-                x = x.replace("Jul", "July")
-                    
-            if "August" in x:
-                continue                
-            
-            elif "Aug" in x:
-                x = x.replace("Aug", "August")
-            if "September" in x:
-                continue                
-            
-            elif "Sep" in x:
-                x = x.replace("Sep", "September")
-            if "October" in x:
-                continue                
-            
-            elif "Oct" in x:
-                x = x.replace("Oct", "October")
-            if "November" in x:
-                continue                
-            
-            elif "Nov" in x:
-                x = x.replace("Nov", "November")                    
-            if "December" in x:
-                continue                    
-            elif "Dec" in x:
-                x = x.replace("Dec", "December")
-            y = (datetime.datetime.strptime(x, '%d/%B/%Y').strftime('%A'))
-            for day in Daytracker.keys():
-                if y == day:
-                    Daytracker[day]+=1
-        else:
-            continue
+  Daytracker[day]+=1
+  Monthdict[date.month]+= 1
+  Monthrepo[date.month].append(line)
+
+for month, line in Monthrepo.items():
+  name = monthlist[month]
+  writer = open(name, 'w')
+  writer.writelines(line)
+  writer.close()
+
+  
 
 
-errors = 100 * float(errors)/float(TotalNumber)                    
+errors = float(100 * float(errors)/float(TotalNumber))                    
             
-redirects = 100 * float(redirects)/float(TotalNumber) 
+redirects = float(100 * float(redirects)/float(TotalNumber))
 
-maximum = max(mostrequested, key=mostrequested.get)
-minumum = min(mostrequested, key=mostrequested.get)
-              
+
+print("\n")              
 print("The total number of request made in the time period represented by the log is %d" %(TotalNumber))
 print("\n")
 print("The following are the number of requests made in each month")
 for key, value in Monthdict.items():
-    print(key, value)
+  print(oldmonth.get(key), value)
+
+
+maximum = max(popularity.items())
+minimum = min(popularity.items())
 print("\n")    
-print("The following are the number of requests made on each day")
 for key, value in Daytracker.items():
     print(key, value)
 print("\n")    
-print("The following is the percentage of errors in the log file: %d%%" %(errors))
+print("The following is the percentage of errors in the log file: %.2f%%" %(errors))
 print("\n")
-print("The following is the percentage of redirects in the log file: %d%%" %(redirects))
+print("The following is the percentage of redirects in the log file: %.2f%%" %(redirects))
 print("\n")
 print("The following is the most requested file:")
-print(maximum)
-print("The following is the least requested file:")
-print(minumum)
-
-print(mostrequested)
+print (max(popularity, key=popularity.get))
+print("The following are the least requested file:")
+key_min = min(popularity.keys(), key=(lambda k: popularity[k]))
+for a, b in popularity.items():
+  if b == popularity[key_min]:
+    print(a)
